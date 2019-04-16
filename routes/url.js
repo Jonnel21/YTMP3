43cc2909990278a8c52d0cba0d4856fcecde9268
Jonnel21 <ajonnel@gmail.com>
2019-04-15 17:13:15 -0700

    added url to video functionality

--- routes/url.js
+++ routes/url.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const ytdl = require('ytdl-core')
+const fs = require('fs')
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
@@ -6,6 +8,6 @@ router.get('/', (req, res, next) => {
     const value = req.query
     console.log(typeof value)
     res.send(`Converting ${value.URL}!`)
-    
+    ytdl(value.URL).pipe(fs.createWriteStream('video.flv'))
 })
 module.exports = router
