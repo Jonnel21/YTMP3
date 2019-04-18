01f80823a7e5344dbe1639c7ccd6ef8cd6ca9ef2
Jonnel21 <ajonnel@gmail.com>
2019-04-18 12:01:11 -0700

    simplified variable names

--- routes/url.js
+++ routes/url.js
@@ -4,10 +4,9 @@ const fs = require('fs')
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-    //res.send('Data Sent!')
-    const value = req.query
-    console.log(typeof value)
-    res.send(`Converting ${value.URL}!`)
-    ytdl(value.URL).pipe(fs.createWriteStream('video.flv'))
+    const url = req.query.URL
+
+    res.send(`Converting ${url}!`)
+    ytdl(url).pipe(fs.createWriteStream('video.flv'))
 })
 module.exports = router
