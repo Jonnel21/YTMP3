4922369468d6b6ea28a7145cf1f6480f539472f3
jonnel21 <ajonnel@gmail.com>
2019-04-23 22:41:58 -0700

    Converts videos in mp3 format and saves to music folder

--- routes/url.js
+++ routes/url.js
@@ -1,15 +1,32 @@
 const express = require('express')
 const ytdl = require('ytdl-core')
 const fs = require('fs')
+const path = require('path')
 const router = express.Router()
+
+// create folder @ working directory
+const createMusicFolder = () => {
+    fs.stat('./music', (err, stats) => {
+        if (err) fs.mkdir('./music', (err) => {if (err) throw err})
+        else { console.log(stats) }
+    })
+}
+createMusicFolder()
 
 router.get('/', (req, res, next) => {
     const url = req.query.URL
-    res.send(`Converting ${url}!`)
-
+    
     ytdl.getInfo(url, (err, info) => {
         const title = info.title
-        ytdl(url).pipe(fs.createWriteStream(title.replace(/\W/g, ' ')))
+        const audioFormats = ytdl.filterFormats(info.formats, 'audioonly')
+        const filename = title.replace(/\W/g, ' ') + '.mp3'
+
+        for(let value in audioFormats) {
+            console.log(audioFormats[value].container)
+        }
+        console.log(`Formats with only audio: ${audioFormats.length}`)
+        ytdl(url, {filter: (format) => format.container === 'm4a'}).pipe(fs.createWriteStream(path.join('./music', filename)))
     })
+    res.send(`Converting ${url}!`)
 })
 module.exports = router
